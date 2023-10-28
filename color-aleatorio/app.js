6932ca145ac9fd9bccec41ce7c3d75a6a5477058
Angaco <109466287+AnGaCo@users.noreply.github.com>
2023-10-27 22:08:11 -0300

    funcionalidad del archivo app.js para crear esa interactividad

---
+++ color-aleatorio/app.js
@@ -0,0 +1,7 @@
+//Seleccionar los elementos del DOM
+const boton = document.querySelector(`button`); //eso nos va a retornar el primer elemento con la etiqueta button
+const color = document.getElementById(`color`); //obtener el id color de este elemento→p en html
+function generarColorExAleatorio() {
+  let digitos = `123456789ABCDEF`;
+  let color = `#`;  
+} 
