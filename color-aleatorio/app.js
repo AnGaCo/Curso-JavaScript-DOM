908b96e8c6755dd24b93130347f9a035d5353589
Angaco <109466287+AnGaCo@users.noreply.github.com>
2023-10-28 23:50:26 -0300

    Final proyecto color aleatorio

--- color-aleatorio/app.js
+++ color-aleatorio/app.js
@@ -1,7 +1,24 @@
 //Seleccionar los elementos del DOM
-const boton = document.querySelector(`button`); //eso nos va a retornar el primer elemento con la etiqueta button
-const color = document.getElementById(`color`); //obtener el id color de este elemento→p en html
-function generarColorExAleatorio() {
-  let digitos = `123456789ABCDEF`;
-  let color = `#`;  
-} 
+const boton = document.querySelector("button"); //eso nos va a retornar el primer elemento con la etiqueta button
+const color = document.getElementById("color"); //obtener el id color de este elemento→p en html
+
+function generarColorExAleatorio(){
+  let digitos = "0123456789ABCDEF";
+  let colorEx = "#";  
+  
+  for (let i = 0; i < 6; i++) {
+    let indiceAleatorio = Math.floor(Math.random()*16);
+    colorEx += digitos[indiceAleatorio];     
+  } 
+  
+  return colorEx;
+}  
+
+boton.addEventListener("click", function() {
+  let colorAleatorio = generarColorExAleatorio();
+  // Actualizar el texto
+  color.textContent = colorAleatorio;
+  // Actualizar el color de fondo 
+  document.body.style.backgroundColor = colorAleatorio; 
+});
+ 
