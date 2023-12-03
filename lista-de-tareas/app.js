c581522146add8489198e24c82d6dba6be082095
Angaco <109466287+AnGaCo@users.noreply.github.com>
2023-12-02 22:55:37 -0300

    Incorporando estructura y estilos a js

---
+++ lista-de-tareas/app.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('ingresar-tarea');
+const boton = document.querySelector('button');
+const listaDeTarea = document.getElementById('lista-de-tareas');
+
+function agregarTarea(){
+  if(input.value) {
+    //Cerear tarea 
+    let tareaNueva = document.createElement('div');
+    tareaNueva.classList.add('tarea');
+
+  }
+}
